91ffc75d82834b417994b872e32d2d2c652fbac4
Catmaster4029 <riccardomatteomanc@libero.it>
2023-02-25 12:50:19 +0100

    Revert "nothing"
    This reverts commit 73fe64cd4ae45638866299ef38be7af012147d57.

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Assets.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Assets.java
@@ -297,6 +297,5 @@ public class Assets {
 		public static final String SPIRIT_HAWK= "sprites/spirit_hawk.png";
 		public static final String RED_SENTRY= "sprites/red_sentry.png";
 		public static final String LIGHT_ORB= "sprites/light_orb.png";
-		public static final String LIGHT_ORB2= "sprites/light_orb.png";
 	}
 }
